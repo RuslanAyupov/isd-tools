87b1e89f6f8d824a6277b8ae15c7024aa480e937
Ruslan Ayupov <ruslan.ayupov@rayupov.ru>
2018-07-15 20:10:51 +0300

    refactoring new UI;
    added - ln18 support;

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,12 +1,14 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { IsdtoolsAlarmModule } from './alarm/alarm.module';
+import { IsdtoolsIsdcalcModule } from './isdcalc/isdcalc.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
         IsdtoolsAlarmModule,
+        IsdtoolsIsdcalcModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
